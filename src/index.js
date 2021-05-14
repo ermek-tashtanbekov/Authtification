2aeafe3ed17f441493fed296a0abe20dd3ce77f6
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-14 21:10:22 +0600

    install react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import auth from '././store/redusers/auth'
 
+const store = createStore(auth)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
